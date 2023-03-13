9dad501a2917400e63ff941a32d799f957b8920d
junnn0021 <junnn0021@gmail.com>
2023-03-13 10:52:20 +0900

    [add]구매내역 인증 기능 구현

--- purchase-auth/index.js
+++ purchase-auth/index.js
@@ -1,9 +1,23 @@
-exports.handler = async (event) => {
-  const email = event.email;
-  console.log(email);
-};
+//exports.handler = async (event) => {
+// const email = event.email;
+//  console.log(email);
+//};
 
-const {
-  connectDb,
-  queries: { getProduct, setStock },
-} = require("./database");
+const mysql = require('mysql2/promise');
+
+exports.handler = async (event) => {
+  const connection = await mysql.createConnection({
+    host: process.env.rds_host,
+    user: process.env.rds_user,
+    password: process.env.rds_password,
+    database: process.env.rds_db_name
+  });
+  
+  const [rows] = await connection.execute('SELECT * FROM Purchasedetails WHERE Customer_Email = ?', [event.email]);
+  console.log(rows)
+  if (rows.length > 0) {
+    return { result: '구매내역 인증에 성공하셨습니다.' };
+  } else {
+    return { result: '구매내역 인증에 실패하셨습니다.' };
+  }
+};
