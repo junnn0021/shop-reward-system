7d1ce990dc7f1c7ac87bcfca80bf8597c742108c
Sondoobo <style056811@gmail.com>
2023-03-22 23:19:53 +0900

    [del]IaC 파일 통합을 위한 기존 디렉토리 삭제

--- IaC_production/attendance-2/vpc-endpoint.tf
+++
@@ -1,6 +0,0 @@
-# vpc endpoint 생성
-resource "aws_vpc_endpoint" "attendance_endpoint_j" {
-  vpc_id              = aws_vpc.attendanceVPC_j.id
-  service_name        = "com.amazonaws.ap-northeast-2.dynamodb"
-  vpc_endpoint_type   = "Gateway"
-}
--- IaC_production/attendance-2/vpc.tf
+++
@@ -1,8 +0,0 @@
-# VPC 생성
-resource "aws_vpc" "attendanceVPC_j" {
-  cidr_block       = var.attendanceVPC_cidr_block
-
-  tags = {
-    Name = "attendanceVPC_j"
-  }
-}
